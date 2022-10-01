6cca8236bd0a4b8b6b1eb89911ff0fc6fb5bef8e
lidoror <lidororanim@gmail.com>
2022-10-01 18:19:13 +0300

    removed unneeded imports

--- src/main/java/com/Oranim/TelegramBot/Main.java
+++ src/main/java/com/Oranim/TelegramBot/Main.java
@@ -6,7 +6,6 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 
-
 public class Main {
 
     public static void main(String[] args) {
