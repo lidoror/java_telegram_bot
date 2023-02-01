e72742f86b6baf022ebb02354ecf5f0895866def
Lidor <lidororanim@gmail.com>
2023-02-01 11:11:32 +0200

    added separator

--- src/main/java/com/oranim/telegrambot/utils/Const.java
+++ src/main/java/com/oranim/telegrambot/utils/Const.java
@@ -1,7 +1,8 @@
 package com.oranim.telegrambot.utils;
 
 public class Const {
-    public final static String SEPARATOR = ";;";
+    public final static String DOUBLE_SEMICOLON_SEPARATOR = ";;";
+    public final static String SINGLE_SPACE_SEPARATOR = " ";
 
 
 }
