9559df0e74afe3cc37865ca187dd608cd967b6b4
Lidor <lidororanim@gmail.com>
2023-07-05 11:51:47 +0300

    added logging to the start of the bot

--- src/main/java/com/oranim/telegrambot/Main.java
+++ src/main/java/com/oranim/telegrambot/Main.java
@@ -1,6 +1,8 @@
 package com.oranim.telegrambot;
 
+import com.oranim.telegrambot.db.MessagesDAO;
 import com.oranim.telegrambot.messageHandler.Bot;
+import com.oranim.telegrambot.utils.BotLogging;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
@@ -13,11 +15,17 @@ public class Main {
         try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
             botsApi.registerBot(new Bot());
+            BotLogging.setInfoLog("_____Bot is Starting_____");
         } catch (TelegramApiException e) {
+            BotLogging.setInfoLog("_____Bot Failed To Start_____");
             e.printStackTrace();
         }catch (Exception e){
+            BotLogging.setInfoLog("_____Bot Failed To Start_____");
             System.out.println(e.getMessage());
+
         }
     }
+
+
 }
 
