b2ee9ed4db5304d99c322bbdfa9bca65f4317698
Lidor <lidororanim@gmail.com>
2023-07-05 11:50:57 +0300

    changed log to be more readable

--- src/main/java/com/oranim/telegrambot/utils/BotLogging.java
+++ src/main/java/com/oranim/telegrambot/utils/BotLogging.java
@@ -7,10 +7,11 @@ public class BotLogging {
     private static final Logger logger = Logger.getLogger(BotLogging.class.getName());
 
     public static void setInfoLog(String log){
-        logger.log(Level.INFO,log);
+        logger.log(Level.INFO,"\n" + "\u001B[32m" + log + "\u001B[0m" + "\n");
+
     }
 
     public static void setCriticalLog(String log){
-        logger.log(Level.WARNING,log);
+        logger.log(Level.WARNING,"\n" + "\u001B[31m" + log + "\u001B[0m" + "\n");
     }
 }
