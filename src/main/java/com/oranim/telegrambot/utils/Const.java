37ed818e5307b5e0e18f8984c08706cb3cab0543
Lidor <lidororanim@gmail.com>
2023-07-24 23:50:59 +0300

    added new consts

--- src/main/java/com/oranim/telegrambot/utils/Const.java
+++ src/main/java/com/oranim/telegrambot/utils/Const.java
@@ -3,6 +3,7 @@ package com.oranim.telegrambot.utils;
 public class Const {
     public final static String DOUBLE_SEMICOLON_SEPARATOR = ";;";
     public final static String SINGLE_SPACE_SEPARATOR = " ";
+    public final static String WHOLE_AND_DECIMAL_NUMBER_REGEX = "-?\\d+(\\.\\d+)?";
 
 
 }
