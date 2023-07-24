d05ff2273c63c09d68c44ffe19df7a8b0c4f9758
Lidor <lidororanim@gmail.com>
2023-07-24 23:53:48 +0300

    changed logger implementation to slf4j

--- src/main/java/com/oranim/telegrambot/utils/BotLogging.java
+++ src/main/java/com/oranim/telegrambot/utils/BotLogging.java
@@ -1,17 +1,32 @@
 package com.oranim.telegrambot.utils;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class BotLogging {
-    private static final Logger logger = Logger.getLogger(BotLogging.class.getName());
+    private static final Logger logger = LoggerFactory.getLogger(BotLogging.class);
 
-    public static void setInfoLog(String log){
-        logger.log(Level.INFO,"\n" + "\u001B[32m" + log + "\u001B[0m" + "\n");
-
+    private static void setInfoLog(String log){
+        logger.info("\n \u001B[32m %s \u001B[0m \n".formatted(log));
     }
 
-    public static void setCriticalLog(String log){
-        logger.log(Level.WARNING,"\n" + "\u001B[31m" + log + "\u001B[0m" + "\n");
+    private static void setWarnLog (String log) {
+        logger.info("\n \u001B[33m %s \u001B[0m \n".formatted(log));
+    }
+
+    private static void setCriticalLog(String log){
+        logger.warn("\n \u001B[31m %s \u001B[0m \n".formatted(log));
+    }
+
+    public static void createLog(LogWarningLevel logStatus , String className, String methodName , String description) {
+        String log = "%s log-> \nClass name: [%s] \nMethod name: [%s] \nDescription: [%s]".formatted(logStatus,className, methodName, description);
+
+        switch (logStatus) {
+            case WARN -> setWarnLog(log);
+            case CRITICAL -> setCriticalLog(log);
+            case INFO -> setInfoLog(log);
+        }
+
     }
 }
